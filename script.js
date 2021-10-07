a0574ace295e50fb725200b25cb2390aea4f9e1b
RajuRk <45646335+RajuRk@users.noreply.github.com>
2021-10-07 22:50:37 +0530

    code updated

--- script.js
+++ script.js
@@ -2,48 +2,34 @@ const firstName = document.getElementById("firstName");
 const lastName = document.getElementById("lastName");
 const address = document.getElementById("address");
 const pinCode = document.getElementById("pinCode");
-const gender = document.getElementById("gender");
-const food =  document.getElementById("food");
+const gender = document.querySelectorAll("#gender");
+const food =  document.querySelectorAll("#food");
 const state = document.getElementById("state");
 const country = document.getElementById("country");
-const submitBtn = document.querySelector(".submitBtn");
-const errorMsg = document.querySelectorAll("#error");
+const errorMsg = document.querySelector("#error");
 
-
-submitBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    formValidation();
+const submitBtn = () => {
+    validation();
     getData();
-});
-
-const checkRadio = () => {
-    const radio = document.querySelectorAll("#gender");
-    var radioValue = [];
-    for(var i=0; i<radio.length; i++) {
-        if(radio[i].checked){
-            radioValue.push(radio[i].value);
-        }
-    }
-    return radioValue;
-}
-
-const checkCheckBox = () => {
-    var checkBox = document.querySelectorAll("#food");
-    var getCheckValue = []
-    for(var i=0; i<checkBox.length; i++){
-        if(checkBox[i].checked){
-            getCheckValue.push(checkBox[i].value);
-        }
-    }
-    return getCheckValue;
-}
-
-const formValidation = () => {
-   if(firstName.value == "" || firstName.value.length >= 2){
-        errorMsg.innerText = "Fields must be filled out";
-        return false;
-   }
+    reset();
 };
+
+const checkValue = (data) => {
+    var getValue = [];
+    for(var i=0; i<data.length; i++) {
+        if(data[i].checked){
+            getValue.push(data[i].value);
+        }
+    }
+    return getValue;
+}
+
+const validation = () => {
+    if(firstName.value == ""){
+        errorMsg.innerHTML = "Error";
+    }
+}
+
 const getData = () =>{
     var table = document.getElementById("list").getElementsByTagName("tbody")[0];
     const formData = {};
@@ -51,20 +37,11 @@ const getData = () =>{
     formData["lastName"] = lastName.value;
     formData["address"] = address.value;
     formData["pinCode"] = pinCode.value;
-    formData["gender"] = `${checkRadio()}`;
-    formData["food"] = `${checkCheckBox()}`;
+    formData["gender"] = `${checkValue(gender)}`;
+    formData["food"] = `${checkValue(food)}`;
     formData["state"] = state.value;
     formData["country"] = country.value;
      
-    // Object.keys(formData).forEach((key) =>{
-    //     if(formData[key] == ""){
-    //         return false;
-    //     }else{
-            
-            
-    //     }
-    // });
-
     table.innerHTML += `<tr>
                             <td>${formData.firstName}</td>
                             <td>${formData.lastName}</td>
@@ -75,9 +52,6 @@ const getData = () =>{
                             <td>${formData.state}</td>
                             <td>${formData.country}</td>
                         </tr>`;
-    
-    reset();
-       
 };
 
 const reset = () =>{
@@ -85,8 +59,9 @@ const reset = () =>{
     lastName.value = "";
     address.value = "";
     pinCode.value = "";
-    gender.value.checked = false;
-    food.value.checked = false;
     state.value = "";
     country.value = "";
+    gender.value = "";
+    food.value = "";
+    
 }
